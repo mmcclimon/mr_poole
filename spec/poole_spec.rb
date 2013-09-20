1bd019b97a16821c16ddf99f3924445db768402b
Michael McClimon <michael@mcclimon.org>
2013-09-20 08:01:47 -0400

    Refactoring spec a bit, adding spec_helper defs

--- spec/poole_spec.rb
+++ spec/poole_spec.rb
@@ -7,50 +7,32 @@ require 'stringio'
 
 module MrPoole
   describe CLI do
-    context 'no jekyll directory' do
-      before :all do
-        # make a directory to work in
-        @olddir = Dir.pwd()
-        @dir = Dir.mktmpdir('nojekyll')
-        Dir.chdir(@dir)
-      end
 
-      after :all do
-        Dir.chdir(@olddir)
-        FileUtils.rm_rf(@dir)
-      end
+    context 'should determine jekyll dir correctly' do
 
       it 'should exit with no _posts directory' do
+        olddir, tmpdir = make_no_jekyll_dir
+
         argv = []
-        lambda {
-          cli = CLI.new(argv)
-        }.should raise_error SystemExit
-      end
-    end   # end context 'no jekyll'
+        output = capture_stdout do
+          lambda { cli = CLI.new(argv) }.should raise_error SystemExit
+        end
 
-    context 'in jekyll directory' do
-      before :each do
-        # make a directory to work in
-        @olddir = Dir.pwd()
-        @dir = Dir.mktmpdir('jekyll')
-        posts = File.join(@dir, '_posts')
-        Dir.mkdir(posts)
-        Dir.chdir(@dir)
+        clean_tmp_files(tmpdir, olddir)
       end
 
-      after :each do
-        Dir.chdir(@olddir)
-        FileUtils.rm_rf(@dir)
-      end
+      it 'should not exit with _posts directory' do
+        olddir, tmpdir = make_jekyll_dir
 
-      it "should not exit with _posts directory" do
         argv = []
         lambda {
           cli = CLI.new(argv)
         }.should_not raise_error
+
+        clean_tmp_files(tmpdir, olddir)
       end
 
-    end   # end context 'in jekyll'
+    end   # end context
 
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,39 @@
 #!/usr/bin/env ruby
 
 require 'stringio'
+require 'tmpdir'
+require 'fileutils'
 
+def capture_stdout(&block)
+  stdout = $stdout
+  fake_out = StringIO.new
+  $stdout = fake_out
+  begin
+    yield
+  ensure
+    $stdout = stdout
+  end
+  fake_out.string
+end
+
+def make_no_jekyll_dir
+  olddir = Dir.pwd()
+  newdir = Dir.mktmpdir('nojekyll')
+  Dir.chdir(newdir)
+  return olddir, newdir
+end
+
+def make_jekyll_dir
+  olddir = Dir.pwd()
+  newdir = Dir.mktmpdir('jekyll')
+  posts = File.join(newdir, '_posts')
+  Dir.mkdir(posts)
+  Dir.chdir(newdir)
+  return olddir, newdir
+
+end
+
+def clean_tmp_files(tmpdir, restoredir)
+  Dir.chdir(restoredir)
+  FileUtils.rm_rf(tmpdir)
+end
